70777e8161cf4d362af48d6fb1d5fa46d93d22d5
Balaganesh S <balaganesh@juspay.in>
2024-09-10 13:34:26 +0530

    Updated HyperSDK version

--- Constants/JuspayConstants.m
+++ Constants/JuspayConstants.m
@@ -7,7 +7,7 @@
 @implementation JuspayConstants
 
 NSString *const Juspay_HYPER_SERVICE_PREFIX = @"in.juspay.";
-NSString *const Juspay_HYPER_TENANT_ID = @"Juspay";
+NSString *const Juspay_HYPER_TENANT_ID = @"juspay";
 NSString *const Juspay_HYPER_RELEASE_CONFIG_URL = @"https://%@assets.juspay.in/hyper/bundles/in.juspay.merchants/%@/ios/%@/release-config.json?toss=%ld";
 
 @end
